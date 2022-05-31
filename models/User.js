b06494aaa39f5935b17164fd3b5a8a19c10ab53f
Yorch82 <jcampo1982@outlook.com>
2022-05-31 13:12:25 +0200

    Coleccion user y endpoint crear user

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    mail: String,
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+module.exports = Product;
