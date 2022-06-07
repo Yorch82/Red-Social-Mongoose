9bd0c9679292b57fc4ee68da0d75750d82bbb9a5
Yorch82 <jcampo1982@outlook.com>
2022-06-07 12:53:41 +0200

    rellenos seeders posts

--- seeders/posts.js
+++ seeders/posts.js
@@ -1,88 +1,75 @@
 const posts = [
 	{
-		title: 'Title 1',
-		content: 'Content 1',
+		title: 'Pecador',
+		content: 'Pupita te voy a borrar el cerito a gramenawer te voy a borrar el cerito. La caidita mamaar ese hombree fistro no puedor.',
         avatar:'../assets/defaultavatar.jpg',
            
 	},
-	{
-		id: 2,
-		title: 'Obi Wan Kenobi',
-		content: 'lasthope@email.com',
+	{		
+		title: 'Fistro',
+		content: 'Lorem fistrum pupita quietooor va usté muy cargadoo va usté muy cargadoo ese hombree ese hombree qué dise usteer ese que llega torpedo quietooor.',
         avatar:'../assets/defaultavatar.jpg',
         		
 	},
-	{
-		id: 3,
-		title: 'Rey Skywalker',
-		content: 'sucks@sucks.com',
+	{		
+		title: 'Gromenauer',
+		content: 'sucks@Hasta luego Lucas apetecan pecador qué dise usteer ese pedazo de no te digo trigo por no llamarte Rodrigor tiene musho peligro ahorarr diodeno.com',
         avatar:'../assets/defaultavatar.jpg',
         		
 	},
-	{
-		id: 4,
-		title: 'John Don',
-		content: 'John@John.com',
+	{		
+		title: 'Lucas',
+		content: 'Te va a hasé pupitaa te va a hasé pupitaa apetecan hasta luego Lucas benemeritaar se calle ustée no puedor de la pradera se calle ustée.',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 5,
-		title: 'John Don',
-		content: 'John@John.com',
+    {		
+		title: 'Acandemor',
+		content: 'Ahorarr te va a hasé pupitaa te va a hasé pupitaa fistro a wan se calle ustée pupita no puedor a gramenawer al ataquerl. ',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 6,
-		title: 'John Don',
-		content: 'John@John.com',
+    {		
+		title: 'Fistro',
+		content: 'Ese hombree está la cosa muy malar te va a hasé pupitaa te voy a borrar el cerito tiene musho peligro qué dise usteer está la cosa muy malar la caidita ahorarr.',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 7,
+    {		
 		title: 'John Don',
-		content: 'John@John.com',
+		content: 'A peich no te digo trigo por no llamarte Rodrigor benemeritaar jarl amatomaa mamaar tiene musho peligro ese hombree se calle ustée ese pedazo de.',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 8,
+    {		
 		title: 'John Don',
-		content: 'John@John.com',
+		content: 'Lorem fistrum no te digo trigo por no llamarte Rodrigor te va a hasé pupitaa quietooor. No puedor hasta luego Lucas a gramenawer amatomaa llevame al sircoo a wan sexuarl ahorarr está la cosa muy malar va usté muy cargadoo de la pradera. ',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 9,
-		title: 'John Don',
-		content: 'John@John.com',
+    {		
+		title: 'Duodeno',
+		content: 'De la pradera condemor ese que llega a wan de la pradera quietooor ese pedazo de está la cosa muy malar. ',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 10,
-		title: 'John Don',
-		content: 'John@John.com',
+    {		
+		title: 'Gromenauer',
+		content: 'Ese que llega mamaar quietooor tiene musho peligro quietooor diodeno.',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 11,
+    {		
 		title: 'John Don',
-		content: 'John@John.com',
+		content: 'A gramenawer de la pradera mamaar sexuarl tiene musho peligro benemeritaar ahorarr.',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 12,
+    {		
 		title: 'John Don',
-		content: 'John@John.com',
+		content: 'Al ataquerl ahorarr torpedo va usté muy cargadoo al ataquerl a peich diodenoo de la pradera a wan no puedor. ',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 13,
-		title: 'John Don',
-		content: 'John@John.com',
+    {		
+		title: 'Pecador',
+		content: 'Lorem fistrum a gramenawer te voy a borrar el cerito va usté muy cargadoo qué dise usteer tiene musho peligro hasta luego Lucas.',
         avatar:'../assets/defaultavatar.jpg'
 	},
-    {
-		id: 14,
-		title: 'John Don',
-		content: 'John@John.com',
+    {		
+		title: 'Benemeritarr',
+		content: 'Te va a hasé pupitaa llevame al sircoo benemeritaar va usté muy cargadoo quietooor jarl sexuarl benemeritaar. ',
         avatar:'../assets/defaultavatar.jpg'
 	},
 ];
